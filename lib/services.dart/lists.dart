f9d2038d4047c56b042c821b18ed164102205829
Pranan Jaya Surya Irnadi <ppranann@gmail.com>
2023-11-19 11:35:33 +0700

    Initial commit

---
+++ lib/services.dart/lists.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+List habitList = [
+  [
+    false, 
+    "Drink Water", 
+    "Drink At Least 8 Cups Of Water", 
+    Icon(Icons.local_drink) 
+  ],
+  [
+    false, 
+    "Read", 
+    "Read at least 25 pages", 
+    Icon(Icons.book) 
+  ],
+  [
+    false, 
+    "Exercise", 
+    "Go to the gym or do cardio", 
+    Icon(Icons.sports_gymnastics), 
+  ],
+  [
+    false, 
+    "Learn Spanish", 
+    "Study Spanish for 20 min", 
+    Icon(Icons.language), 
+  ],
+  [
+    false, 
+    "Code", 
+    "Work on coding project", 
+    Icon(Icons.computer), 
+  ],
+  [
+    false, 
+    "Journal", 
+    "Write Down 3 things that you appreciate", 
+    Icon(Icons.edit), 
+  ],
+];
